38c18192db001809496cd790048d667818a73009
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-09-02 22:23:17 +0900

    remove export

--- src/app/game/flash/page.tsx
+++ src/app/game/flash/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import type { GameData } from "@/types/game";
 
-export const gameData: GameData = {
+const gameData: GameData = {
   title: "フラッシュ暗算",
   description:
     "6口/1桁/5秒のフラッシュ暗算を行います。表示された数字の総和を入力してください。",
