f7314c2cb4ca677d4ed801e4932dc0abbc8b27d6
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-09-06 18:54:45 +0900

    not server

--- src/app/alarm/page.tsx
+++ src/app/alarm/page.tsx
@@ -2,8 +2,6 @@ import { getGameDirectories } from "@/lib/game";
 import Clock from "./_components/clock";
 import RandomGameButton from "./_components/random-game-button";
 
-export const dynamic = "force-dynamic";
-
 export default function Alarm() {
   const games = getGameDirectories();
 
